6c92bfe6136fe0fd78ececeaee473cb381ed22f5
MyrianHF <92150766+MyrianHF@users.noreply.github.com>
2021-10-13 13:18:34 +0200

    Add files via upload

---
+++ PruebaJava.java
@@ -0,0 +1,11 @@
+public class PruebaJava {
+
+	public stativ void main(String args[]) {
+
+		System.out.println("Myrian Herreras Franco");
+		System.out.println("Cembranos 1995");
+		System.out.println("Cembranos - Windows 10");
+
+	}
+
+}
